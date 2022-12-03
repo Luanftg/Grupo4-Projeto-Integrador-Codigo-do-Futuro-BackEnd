fbbdc13983f5f5d6c927788bdddf5ccff3b16b55
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2022-12-03 09:55:26 -0300

    [FUNCIONALIDADE]: adiciona rotas para CRUD de usuário

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,11 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', homeController.index);
 
-router.get('/user', usersController.index);
-router.post('/user', usersController.create);
+router.get('/users', usersController.index);
+router.post('/users', usersController.create);
+router.get('/users/:id', usersController.findById);
+router.delete('/users/:id', usersController.delete);
+router.put('/users/:id', usersController.update);
 
 
 module.exports = router;

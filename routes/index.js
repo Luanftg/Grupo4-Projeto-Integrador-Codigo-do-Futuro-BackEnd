b8d33891e0eea0630fcc75e9a2c96acdd59ecba9
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2022-12-05 01:44:42 -0300

    [FUNCIONALIDADE]: cria rotas com CRUD de pedidos

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 const homeController = require('../controller/homeController');
 const usersController = require('../controller/usersController');
 const productController = require('../controller/productsController');
+const orderController = require('../controller/ordersController');
 var router = express.Router();
 
 /* GET home page. */
@@ -19,5 +20,10 @@ router.get('/products/:id', productController.findById);
 router.delete('/products/:id', productController.delete);
 router.put('/products/:id', productController.update);
 
+router.get('/orders', orderController.index);
+router.post('/orders', orderController.create);
+router.get('/orders/:id', orderController.findById);
+router.delete('/orders/:id', orderController.delete);
+router.put('/orders/:id', orderController.update);
 
 module.exports = router;
